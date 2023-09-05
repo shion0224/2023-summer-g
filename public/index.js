c674d37175c43eae951de0e6eb0df4a2d1a31c66
katoukaiji <124645223+katoukaiji@users.noreply.github.com>
2023-09-05 15:43:10 +0900

    ページを開いたときにdreamsからデータを持ってくるようにしました

--- public/index.js
+++ public/index.js
@@ -1,16 +1,4 @@
-// When the page loads, fetch the welcome message
 window.onload = async () => {
-  try {
-    const response = await fetch("/welcome-message");
-    const welcomeMessage = await response.text();
-    document.getElementById("welcome-message-container").innerText =
-      welcomeMessage;
-  } catch (error) {
-    console.error("Error fetching welcome message:", error);
-  }
-};
-
-document.getElementById("post-button").onclick = async () => {
   try {
     const response = await fetch("/dreams");
     document.getElementById("post-button").innerText = await response.text();

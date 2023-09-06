99a3bdac16b7e6e56f2bc041215268590913543e
shion0224 <shion8214@gmail.com>
2023-09-06 10:01:00 +0900

    投稿一覧、投稿詳細のバランスを合わせる作業

--- public/index.js
+++ public/index.js
@@ -1,45 +1,51 @@
 window.onload = async () => {
-  try {
-    const response = await fetch("/dreams");
+  /**
+   * /index.htmlでの処理
+   */
+  if (window.location.pathname === "/" ||window.location.pathname === "/index.html") {
+    try {
+      const response = await fetch("/dreams");
 
-    const parentDiv = document.getElementById("get-contents");
+      const parentDiv = document.getElementById("get-contents");
+      for (let i = 1; i <= 5; i++) {
+        // dreams1, dreams2, ... のようなdiv要素を作成
+        const dreamDiv = document.createElement("div");
+        dreamDiv.setAttribute("id", `dreams`);
+        parentDiv.appendChild(dreamDiv);
 
-    for (let i = 1; i <= 10; i++) {
-      // dreams1, dreams2, ... のようなdiv要素を作成
-      const dreamDiv = document.createElement("div");
-      dreamDiv.setAttribute("id", `dreams`);
-      parentDiv.appendChild(dreamDiv);
+        // dream-title1, dream-title2, ... のようなdiv要素をdreamDivの子要素として作成
+        const dreamTitleDiv = document.createElement("div");
+        dreamTitleDiv.setAttribute("id", `dream-title${i}`);
+        dreamTitleDiv.textContent = `This is dream title ${i}`; // サンプルテキスト
+        dreamDiv.appendChild(dreamTitleDiv);
 
-      // dream-title1, dream-title2, ... のようなdiv要素をdreamDivの子要素として作成
-      const dreamTitleDiv = document.createElement("div");
-      dreamTitleDiv.setAttribute("id", `dream-title${i}`);
-      dreamTitleDiv.textContent = `This is dream title ${i}`; // サンプルテキスト
-      dreamDiv.appendChild(dreamTitleDiv);
-
-      // dream-content1, dream-content2, ... のようなdiv要素をdreamDivの子要素として作成
-      const dreamContentDiv = document.createElement("div");
-      dreamContentDiv.setAttribute("id", `dream-content${i}`);
-      dreamContentDiv.textContent = `This is dream content ${i}`; // サンプルテキスト
-      dreamDiv.appendChild(dreamContentDiv);
+        // dream-content1, dream-content2, ... のようなdiv要素をdreamDivの子要素として作成
+        const dreamContentDiv = document.createElement("div");
+        dreamContentDiv.setAttribute("id", `dream-content${i}`);
+        dreamContentDiv.textContent = `This is dream content ${i}`; // サンプルテキスト
+        dreamDiv.appendChild(dreamContentDiv);
+      }
+    } catch (error) {
+      console.error("Error fetching from /post:", error);
     }
-
-  } catch (error) {
-    console.error("Error fetching from /post:", error);
   }
 
-  // try {
-  //   const response = await fetch("/dream-title");
-  //   document.getElementById("dream-title").innerText = await response.text();
-  // } catch (error) {
-  //   console.error("Error fetching from /dream-title:", error);
-  // }
+  if (window.location.pathname === "/post-detail.html") {
+    try {
+      const response = await fetch("/dream-title");
+      document.getElementById("dream-title").innerText = await response.text();
+    } catch (error) {
+      console.error("Error fetching from /dream-title:", error);
+    }
 
-  // try {
-  //   const response = await fetch("/dream-content");
-  //   document.getElementById("dream-content").innerText = await response.text();
-  // } catch (error) {
-  //   console.error("Error fetching from /dream-title:", error);
-  // }
+    try {
+      const response = await fetch("/dream-content");
+      document.getElementById("dream-content").innerText = await response
+        .text();
+    } catch (error) {
+      console.error("Error fetching from /dream-title:", error);
+    }
+  }
 };
 
 document.getElementById("post-button").onclick = async () => {
@@ -54,7 +60,7 @@ document.getElementById("post-button").onclick = async () => {
         { contents: contents, titles: titles },
       ),
     });
-    document.getElementById("test").innerText = await response.text();
+    // document.getElementById("test").innerText = await response.text();
   } catch (error) {
     console.error("Error posting dream:", error);
   }
